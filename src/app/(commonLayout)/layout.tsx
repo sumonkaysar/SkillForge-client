f97994dff530353ed37c3abae9548884a6f473b5
Sumon <hemon.hasan123@gmail.com>
2025-10-10 09:37:43 +0600

    added navbar

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,5 +1,13 @@
+import Navbar from "../../components/shared/Navbar";
+
 const CommonLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className=" flex flex-col h-screen">
+      <Navbar />
+      <div className="h-full">{children}</div>
+      <div>Footer</div>
+    </div>
+  );
 };
 
 export default CommonLayout;
