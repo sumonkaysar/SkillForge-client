10f9901c4de6b9ead3df3eac5f70b01263b13dcb
Sumon <hemon.hasan123@gmail.com>
2025-10-10 09:17:05 +0600

    added some pages

---
+++ src/app/(commonLayout)/about/page.tsx
@@ -0,0 +1,9 @@
+import MyInfo from "@/components/about/MyInfo";
+
+export const revalidate = 3600;
+
+const About = () => {
+  return <MyInfo />;
+};
+
+export default About;
---
+++ src/app/(commonLayout)/layout.tsx
@@ -0,0 +1,5 @@
+const CommonLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default CommonLayout;
