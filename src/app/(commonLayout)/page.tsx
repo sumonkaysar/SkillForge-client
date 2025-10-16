8f9f34bda0ac785620f0daf8578b09d1cf464a85
Sumon <hemon.hasan123@gmail.com>
2025-10-16 18:32:31 +0600

    feat(Home): added projects and blogs section in home

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,13 +1,19 @@
 import About from "@/components/home/About";
 import Banner from "@/components/home/banner/Banner";
+import BlogSection from "@/components/home/BlogsSection";
+import ProjectsSection from "@/components/home/ProjectsSection";
+import Skills from "@/components/home/Skills";
 
-const Home = () => {
+const HomePage = () => {
   return (
     <>
       <Banner />
       <About />
+      <Skills />
+      <ProjectsSection />
+      <BlogSection />
     </>
   );
 };
 
-export default Home;
+export default HomePage;
---
+++ src/types/project.type.ts
@@ -0,0 +1,18 @@
+export interface IProject {
+  _id: string;
+  name: string;
+  title: string;
+  shortDescription: string;
+  description: string;
+  category: "Frontend" | "Backend" | "Fullstack" | "Mobile" | "Other";
+  techStack: string[];
+  image?: string;
+  video?: string;
+  liveDemo?: string;
+  githubFrontend?: string;
+  githubBackend?: string;
+  features: string[];
+  myContribution?: string[];
+  createdAt: string;
+  updatedAt: string;
+}
